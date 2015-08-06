2f9066963731c288e98e82c6c171351271aba8bc
Mayanka <mayanka@prakasheddysmbp.kc.umkc.edu>
2015-08-06 17:06:32 -0500

    QA and WordRecall included

---
+++ WordRecall/VisualRecall/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StroopColorGame
+//
+//  Created by Mayanka  on 7/28/15.
+//  Copyright (c) 2015 umkc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
